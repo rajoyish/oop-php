4c15d35d964aeb2117c5c2c4c176257cc54a478c
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-03 20:25:45 +0545

    Method Arguments

---
+++ 002-class-methods.php
@@ -0,0 +1,17 @@
+<?php
+
+class Product
+{
+    public $name = 'soap';
+    public $price = 100;
+
+    public function priceAsCurrency($currencySymbol = '')
+    {
+        $priceAsCurrency = $this->price / 100;
+
+        return $currencySymbol.$priceAsCurrency;
+    }
+}
+
+$product = new Product();
+print $product->priceAsCurrency('₹').PHP_EOL;
