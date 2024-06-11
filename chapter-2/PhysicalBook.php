0fa5f129b01ea80b7530d0ee6005352b092ae7a7
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-11 07:42:21 +0545

    Inheritance and Constructors

--- chapter-2/PhysicalBook.php
+++ chapter-2/PhysicalBook.php
@@ -4,17 +4,23 @@ require_once 'Book.php';
 
 class PhysicalBook extends Book
 {
-    public $weight;
+  public $weight;
 
-    public function getWeight(): int
-    {
-        return $this->weight;
-    }
+  public function __construct(string $title, string $author, int $price, int $weight)
+  {
+    parent::__construct($title, $author, $price);
+    $this->weight = $weight;
+  }
 
-    public function print(): string
-    {
-        return "{$this->title}, {$this->author}, {$this->price} kg";
-    }
+  public function getWeight(): int
+  {
+    return $this->weight;
+  }
+
+  public function print(): string
+  {
+    return "{$this->title}, {$this->author}, {$this->price} kg";
+  }
 
 
 }
--- chapter-2/inheritance.php
+++ chapter-2/inheritance.php
@@ -2,7 +2,8 @@
 require_once 'PhysicalBook.php';
 require_once 'DigitalBook.php';
 
-$physicalBook = new PhysicalBook('A Random Book', 'Jane Doe', 2000);
+$physicalBook = new PhysicalBook('A Random Book', 'Jane Doe', 2000, 300);
 $physicalBook->price = 500;
 
-print $physicalBook->getPrice() . PHP_EOL;
+print $physicalBook->getTitle() . PHP_EOL;
+print $physicalBook->print() . PHP_EOL;
