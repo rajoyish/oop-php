0c8f3336b5303cd684ee2e9770d5dad4735b163d
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-20 08:17:24 +0545

    Combining Traits and Interfaces

---
+++ chapter-4/IdentifiableTrait.php
@@ -0,0 +1,9 @@
+<?php
+
+trait IdentifiableTrait
+{
+  public function getHexId(): string
+  {
+    return bin2hex(random_bytes(5));
+  }
+}
---
+++ chapter-4/Song.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'Identifiable.php';
+require_once 'IdentifiableTrait.php';
+
+class Song implements Identifiable
+{
+  use IdentifiableTrait;
+}
