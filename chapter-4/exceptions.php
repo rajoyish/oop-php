aabd42c8e649e62cb2311fddd36a618de0057226
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-23 09:10:08 +0545

    Try / Catch blocks

--- chapter-4/exceptions.php
+++ chapter-4/exceptions.php
@@ -1,6 +1,11 @@
 <?php
 require_once 'JSONFileReader.php';
 
-$reader = new JSONFileReader();
-$items = $reader->readFileAsAssociativeArray('foo.json');
-print_r($items);
+try {
+	$reader = new JSONFileReader();
+	// $items = $reader->readFileAsAssociativeArray('inventory.json');
+	$items = $reader->readFileAsAssociativeArray('foo.json');
+	print_r($items);
+} catch (Exception $exception) {
+	print $exception->getMessage() . PHP_EOL;
+}
