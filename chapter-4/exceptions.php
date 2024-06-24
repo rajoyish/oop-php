59fb578c4b5393ab3825055adadb1cda7eb670e4
Rajesh Budhathoki <rajoyish@gmail.com>
2024-06-24 08:13:25 +0545

    Exception Subclasses

--- chapter-4/exceptions.php
+++ chapter-4/exceptions.php
@@ -2,10 +2,12 @@
 require_once 'JSONFileReader.php';
 
 try {
-	$reader = new JSONFileReader();
-	// $items = $reader->readFileAsAssociativeArray('inventory.json');
-	$items = $reader->readFileAsAssociativeArray('foo.json');
-	print_r($items);
+  $reader = new JSONFileReader();
+  // $items = $reader->readFileAsAssociativeArray('inventory.json');
+  $items = $reader->readFileAsAssociativeArray('foo.json');
+  print_r($items);
+} catch (JsonException $jsonException) {
+  print 'There is a problem with JSON: ' . $jsonException->getMessage() . ' on line ' . $jsonException->getLine() . ' in file ' . $jsonException->getFile() . PHP_EOL;
 } catch (Exception $exception) {
-	print $exception->getMessage() . PHP_EOL;
+  print 'A problem occurred: ' . $exception->getMessage() . ' on line ' . $exception->getLine() . ' in file ' . $exception->getFile() . PHP_EOL;
 }
