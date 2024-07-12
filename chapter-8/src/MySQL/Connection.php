d8643154927da2c5127b44903a10c0632665fa21
Rajesh Budhathoki <rajoyish@gmail.com>
2024-07-12 08:48:22 +0545

    ORM Setup

--- chapter-8/src/MySQL/Connection.php
+++ chapter-8/src/MySQL/Connection.php
@@ -2,11 +2,33 @@
 
 namespace App\MySQL;
 
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\ORMSetup;
+
 class Connection
 {
     private static ?self $instance = null;
 
-    private function __construct() {}
+    private EntityManager $entityManager;
+
+    private function __construct()
+    {
+        $config = ORMSetup::createAttributeMetadataConfiguration(
+            paths: [dirname(__DIR__)],
+            isDevMode: true
+        );
+
+        $dsn = 'pdo-mysql://root@localhost:3306/doctrine-demo?charset=utf8mb4';
+        $dsnParser = new DsnParser();
+        $connectionParams = $dsnParser->parse($dsn);
+
+        $connection = DriverManager::getConnection(
+            params: $connectionParams,
+            config: $config
+        );
+
+        $this->entityManager = new EntityManager($connection, $config);
+    }
 
     public static function getInstance(): self
     {
@@ -15,5 +37,10 @@ class Connection
         }
 
         return self::$instance;
+    }
+
+    public function getEntityManager(): EntityManager
+    {
+        return $this->entityManager;
     }
 }
