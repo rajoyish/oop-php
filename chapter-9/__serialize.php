238a6d8f330f805583d8759e4b823f03ceef9bb4
Rajesh Budhathoki <rajoyish@gmail.com>
2024-07-18 08:45:56 +0545

    __unserialize Magic Method

--- chapter-9/__serialize.php
+++ chapter-9/__serialize.php
@@ -7,4 +7,5 @@ require_once 'vendor/autoload.php';
 $user = new User(username: 'Milo18@yahoo.com', password: 'password11');
 $serializedUser = serialize($user);
 
-dump($serializedUser);
+$unSerializedUser = unserialize($serializedUser);
+dump($unSerializedUser);
--- chapter-9/src/User.php
+++ chapter-9/src/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 class User
 {
-    public function __construct(private string $username, private string $password) {}
+    public function __construct(public string $username, protected string $password) {}
 
     public function __serialize(): array
     {
@@ -12,5 +12,25 @@ class User
             'user' => $this->username,
             'pass' => $this->password,
         ];
+    }
+
+    public function __sleep(): array
+    {
+        echo 'Going to sleep...';
+
+        return ['username', 'password'];
+    }
+
+    public function __wakeup(): void
+    {
+        echo 'Waking up...';
+    }
+
+    public function __unserialize(array $data): void
+    {
+        $this->username = $data['user'];
+        $this->password = $data['pass'];
+
+        echo 'Doing anything else which is necessary...'.PHP_EOL;
     }
 }
