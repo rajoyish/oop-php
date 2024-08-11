f29db6cefed8f11b11cd8f2fb71b708849163cee
Rajesh Budhathoki <rajoyish@gmail.com>
2024-08-11 08:15:06 +0545

    Creating Custom Attributes & Applying Attributes

--- attributes/src/DTO/UserRegistration.php
+++ attributes/src/DTO/UserRegistration.php
@@ -2,10 +2,15 @@
 
 namespace App\DTO;
 
+use App\Validation\Rules\Required;
+
 readonly class UserRegistration
 {
     public function __construct(
+        #[Required]
         public string $username,
+
+        #[Required]
         public string $email
     ) {}
 }
---
+++ attributes/src/Validation/Rules/Required.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Validation\Rules;
+
+use Attribute;
+
+#[Attribute]
+class Required {}
